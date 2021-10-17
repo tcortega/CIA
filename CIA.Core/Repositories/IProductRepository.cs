dee92d805e6eab83f2a3ea693263bab0f4613c0e
tcortega <tarcisiocaetano2009@gmail.com>
2021-10-17 17:55:25 -0300

    Creating Product and StoreProduct repository.

---
+++ CIA.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CIA.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIA.Core.Repositories
+{
+    /// <summary>
+    /// Stores records.
+    /// </summary>
+    public interface IProductRepository : IRepository<ProductEntity>
+    {
+    }
+}
